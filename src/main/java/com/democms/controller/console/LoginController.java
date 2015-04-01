f05accdc5cbe430e86bee7ebc9d80a224c1c5ced
Simon <simon.cao@ceosoftcenters.com>
2015-04-01 17:24:53 +0800

    Update: log4j2 hibernate

--- src/main/java/com/democms/controller/console/LoginController.java
+++ src/main/java/com/democms/controller/console/LoginController.java
@@ -2,6 +2,8 @@ package com.democms.controller.console;
 
 import javax.annotation.Resource;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,11 +14,18 @@ import com.democms.service.console.ConsoleLoginService;
 @RequestMapping("/console")
 public class LoginController {
 	
+	private static Logger logger = LogManager.getLogger(LoginController.class);
+	
 	@Resource
 	private ConsoleLoginService consoleLoginServiceImpl;
 
 	@RequestMapping("**")
 	public String consoleLoginPage(){
+		logger.info("log info");
+		logger.warn("log warn");
+		logger.debug("log debug");
+		logger.error("log error");
+		logger.trace("log trace");
 		return "login";
 	}	
 	
