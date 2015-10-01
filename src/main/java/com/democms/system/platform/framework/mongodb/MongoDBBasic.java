08f31dd12c4dc599b6a21c97d44b8d65ee9a3dea
FIFTH <caoyp-01@163.com>
2015-10-01 09:46:10 +0800

    IntelliJ IDEA config & no mongodb

--- src/main/java/com/democms/system/platform/framework/mongodb/MongoDBBasic.java
+++ src/main/java/com/democms/system/platform/framework/mongodb/MongoDBBasic.java
@@ -13,30 +13,30 @@ import java.util.Arrays;
 
 
 public class MongoDBBasic {
-    private static String databaseName = "DemoCMSLog"; //数据库名称
-    private static String host = "localhost";
-    private static String port = "27017";
-    private static String username = "demo"; //用户AK
-    private static String password = "password"; //用户SK
-    private static String serverName = host + ":" + port;
-    
-	public static DB getNewMongoClient() throws UnknownHostException {
-		
-		databaseName = SpringPropertyResourceReader.getProperty("mongodb.name");
-		host = SpringPropertyResourceReader.getProperty("mongodb.url");
-		port = SpringPropertyResourceReader.getProperty("mongodb.port");
-		username = SpringPropertyResourceReader.getProperty("mongodb.user");
-		password = SpringPropertyResourceReader.getProperty("mongodb.password");
-		serverName = host + ":" + port;
-		
-		MongoClient mongoClient = new MongoClient(
-				new ServerAddress(serverName), Arrays.asList(MongoCredential
-						.createMongoCRCredential(username, databaseName,
-								password.toCharArray())),
-				new MongoClientOptions.Builder().cursorFinalizerEnabled(false)
-						.build());
-		DB mongoDB = mongoClient.getDB(databaseName);
-		mongoDB.authenticate(username, password.toCharArray());
-		return mongoDB;
-	} 
+//    private static String databaseName = "DemoCMSLog"; //数据库名称
+//    private static String host = "localhost";
+//    private static String port = "27017";
+//    private static String username = "demo"; //用户AK
+//    private static String password = "password"; //用户SK
+//    private static String serverName = host + ":" + port;
+//
+//	public static DB getNewMongoClient() throws UnknownHostException {
+//
+//		databaseName = SpringPropertyResourceReader.getProperty("mongodb.name");
+//		host = SpringPropertyResourceReader.getProperty("mongodb.url");
+//		port = SpringPropertyResourceReader.getProperty("mongodb.port");
+//		username = SpringPropertyResourceReader.getProperty("mongodb.user");
+//		password = SpringPropertyResourceReader.getProperty("mongodb.password");
+//		serverName = host + ":" + port;
+//
+//		MongoClient mongoClient = new MongoClient(
+//				new ServerAddress(serverName), Arrays.asList(MongoCredential
+//						.createMongoCRCredential(username, databaseName,
+//								password.toCharArray())),
+//				new MongoClientOptions.Builder().cursorFinalizerEnabled(false)
+//						.build());
+//		DB mongoDB = mongoClient.getDB(databaseName);
+//		mongoDB.authenticate(username, password.toCharArray());
+//		return mongoDB;
+//	}
 }
