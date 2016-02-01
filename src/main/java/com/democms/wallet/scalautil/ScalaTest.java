786ab1dc801ddd6183411d9c63ffc34d337377df
Simon <simon.cao@ceosoftcenters.com>
2016-02-01 16:10:10 +0800

    scala support

---
+++ src/main/java/com/democms/wallet/scalautil/ScalaTest.java
@@ -0,0 +1,16 @@
+package com.democms.wallet.scalautil;
+
+import com.democms.test.TestScala;
+
+/**
+ *
+ */
+public class ScalaTest {
+
+    public static void main(String[] args){
+        TestScala test = new TestScala();
+        //System.out.println(test.testFunction());
+        test.scalaPrint();
+
+    }
+}
---
+++ src/main/scala/com/democms/test/TestScala.scala
@@ -0,0 +1,10 @@
+package com.democms.test
+
+/**
+  *
+  */
+class TestScala {
+    def testFunction = "hello world"
+
+    def scalaPrint = System.out.println("hello world")
+}
